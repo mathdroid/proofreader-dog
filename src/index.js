c0666cbad7bb22958afc477534167344bb927925
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2017-03-19 13:13:17 +0700

    add stickers

--- src/index.js
+++ src/index.js
@@ -29,12 +29,13 @@ dog.on('text', async event => {
         const {displayName} = await dog.getProfileFromEvent(event)
         const {replyToken, message: {text}} = event
         const {suggestions, typos} = await lint(text)
-        const replyText = `${displayName}: ${text}`
+        const replyText = `Woof! Hi ${displayName}!\n\nI've read your message:\n\n${text}`
         const replyBalloon = new Messages().addText(replyText)
-        const reasons = suggestions.length && suggestions.map(({reason}) => reason).join('\n')
-        if (reasons) replyBalloon.addText(`Suggestions:\n\n${reasons}`)
-        const words = typos.length && typos.map(({word, suggestions}) => `Error on word "${word}". Did you mean ${suggestions.join('/')}?`).join('\n')
-        if (words) replyBalloon.addText(`Typos:\n\n${words}`)
+        const reasons = suggestions.length && suggestions.map(({reason}) => reason).join('\n\n')
+        if (reasons) replyBalloon.addSticker({packageId: 1, stickerId: 15}).addText(`My dog-sense 🐕 has some suggestions:\n\n${reasons}`)
+        const words = typos.length && typos.map(({word, suggestions}) => `Error on word "${word}". Did you mean ${suggestions.join('/')}?`).join('\n\n')
+        if (words) replyBalloon.addSticker({packageId: 1, stickerId: 10}).addText(`🤔🔥 A bit of typographical errors:\n\n${words}`)
+        if (!reasons && !words) replyBalloon.addSticker({packageId: 1, stickerId: 14}).addText('Woof woof! I can\'t find any errors! 🐶 Nice job!')
         dog.replyMessage(replyToken, replyBalloon.commit())
     } catch ({message}) {
         console.log(message)
